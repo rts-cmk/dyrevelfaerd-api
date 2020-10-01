1a287db553ee395e921544abf1c87d0fab8be355
Brian Emilius <be@rts.dk>
2020-10-01 13:22:54 +0200

    dynamic domain fix

--- routes/asset.route.js
+++ routes/asset.route.js
@@ -1,8 +1,9 @@
-var { createSingleAsset, getAllAssets, getSingleAsset } = require("../controllers/asset.controller");
+var { createSingleAsset, getAllAssets, getSingleAsset, updateSingleAsset } = require("../controllers/asset.controller");
 var { isAuthorized } = require("../middleware/auth");
 
 module.exports = function(router) {
 	router.post("/api/v1/assets", isAuthorized, createSingleAsset);
 	router.get("/api/v1/assets", getAllAssets);
 	router.get("/api/v1/assets/:id", getSingleAsset);
+	router.patch("/api/v1/assets/:id", updateSingleAsset);
 };
